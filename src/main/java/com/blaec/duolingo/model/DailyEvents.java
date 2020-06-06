360d6e048abf079006b52d151f4a625dc4187a31
blaec <odeskonst@yandex.ru>
2020-06-06 12:23:02 +0300

    break monoclass

---
+++ src/main/java/com/blaec/duolingo/model/DailyEvents.java
@@ -0,0 +1,52 @@
+package com.blaec.duolingo.model;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DailyEvents {
+    private final LocalDate date;
+    private final Map<String, Long> dailyXpByType;
+    private final Map<String, Long> dailyCountByType;
+    private Long totalXp;
+
+    private DailyEvents(LocalDate date, String event, Long xp) {
+        this.date = date;
+        this.dailyXpByType = new HashMap<>();
+        this.dailyXpByType.put(event, xp);
+        this.dailyCountByType = new HashMap<>();
+        this.dailyCountByType.put(event, 1L);
+        this.totalXp = xp;
+    }
+
+    public void mergeWith(Event event) {
+        String type = event.getType();
+        Long xp = event.getXp();
+        if (this.dailyXpByType.containsKey(type)) {
+            this.dailyXpByType.replace(type, this.dailyXpByType.get(type) + xp);
+        } else {
+            this.dailyXpByType.put(type, xp);
+        }
+        if (this.dailyCountByType.containsKey(type)) {
+            this.dailyCountByType.replace(type, this.dailyCountByType.get(type) + 1);
+        } else {
+            this.dailyCountByType.put(type, 1L);
+        }
+        this.totalXp += event.getXp();
+    }
+
+    public static DailyEvents ofNew(Event event) {
+        return new DailyEvents(event.getStartDate(), event.getType(), event.getXp());
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%tF | %s | %s | %s | %4d", date, stats("lesson"), stats("practice"), stats("test"), totalXp);
+    }
+
+    private String stats(String event) {
+        Long xp = dailyXpByType.get(event);
+        Long count = dailyCountByType.get(event);
+        return String.format("%4s(%2s) /%3s", xp == null ? "-" : xp, xp == null ? "--" : xp / count, count == null ? "-" : count);
+    }
+}
---
+++ src/main/java/com/blaec/duolingo/model/Event.java
@@ -0,0 +1,59 @@
+package com.blaec.duolingo.model;
+
+import org.json.simple.JSONObject;
+
+import java.time.*;
+
+public class Event {
+    public static final int SECONDS_PER_MINUTE = 60;
+    public static final int SECONDS_PER_HOUR = 3600;
+
+
+    private final LocalDateTime start;
+    private final String type;
+    private final Long xp;
+    private String dif = "";
+
+    public static Event of(JSONObject event) {
+        return new Event(event);
+    }
+
+    private Event(JSONObject event) {
+        Long seconds = (Long) event.get("datetime");
+        this.start = LocalDateTime.ofInstant(
+                Instant.ofEpochMilli(seconds),
+                ZoneId.systemDefault());
+        this.type = (String) event.get("event_type");
+        this.xp = (Long) event.get("improvement");
+    }
+
+    public LocalDateTime getStart() {
+        return start;
+    }
+
+    public void setDif(Duration dif) {
+        long seconds = dif.getSeconds();
+        int hours = (int) (seconds / SECONDS_PER_HOUR);
+        seconds -= hours * SECONDS_PER_HOUR;
+        int minutes = (int) (seconds / SECONDS_PER_MINUTE);
+        seconds -= minutes * SECONDS_PER_MINUTE;
+        this.dif = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    public LocalDate getStartDate() {
+        return start.toLocalDate();
+    }
+
+    public Long getXp() {
+        return xp;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%tF %tT | %8s | %-8s %-2d", start, start, dif, type, xp);
+    }
+}
